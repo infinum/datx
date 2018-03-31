09115244c2334325a40ac86896145863ce08e0fd
Darko Kukovec <darko@kukovec.eu>
2018-03-31 13:31:27 +0200

    Expose esm builds

--- packages/datx/rollup.config.js
+++ packages/datx/rollup.config.js
@@ -1,14 +1,14 @@
 import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
 
+import pkg from './package.json';
+
 export default {
   input: './src/index.ts',
-  output: {
-    file: './dist/index.js',
-    format: 'umd',
-    name: 'datx',
-  },
-
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' },
+  ],
   plugins: [
     typescript({
       check: true,
