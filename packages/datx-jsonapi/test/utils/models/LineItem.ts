5e1b45f1309a6f01680fb1342db7c99fa636ad97
Darko Kukovec <darko@infinum.hr>
2019-03-19 19:23:36 +0100

    Remove some more extra code

--- packages/datx-jsonapi/test/utils/models/LineItem.ts
+++ packages/datx-jsonapi/test/utils/models/LineItem.ts
@@ -1,11 +1,7 @@
-import { IType, Model, prop } from 'datx';
+import { IType, Model } from 'datx';
 
 import { jsonapi } from '../../../src';
-import { ProductVariant } from './ProductVariant';
 
 export class LineItem extends jsonapi(Model) {
   public static type: IType = 'line_items';
-
-  @prop.toOne(ProductVariant) public productVariant!: ProductVariant;
-  @prop.toOne(ProductVariant) public multitrackProductVariant!: ProductVariant;
 }
