16707869c5c6895a7a02a56f82058771694b7098
Jirka Svoboda <jirka@digitaldeals.cz>
2019-04-15 13:12:43 +0200

    Added posibility to specify IJsonApiView model explicitly

--- packages/datx-jsonapi/src/interfaces/IJsonapiView.ts
+++ packages/datx-jsonapi/src/interfaces/IJsonapiView.ts
@@ -5,15 +5,15 @@ import { IJsonapiModel } from './IJsonapiModel';
 import { IRequestOptions } from './IRequestOptions';
 import { IResponse } from './JsonApi';
 
-export interface IJsonapiView extends View {
-  sync<T extends IJsonapiModel = IJsonapiModel>(body?: IResponse): T | Array<T> | null;
+export interface IJsonapiView<T extends IJsonapiModel = IJsonapiModel> extends View<T> {
+  sync(body?: IResponse): T | Array<T> | null;
 
-  fetch<T extends IJsonapiModel = IJsonapiModel>(
+  fetch(
     id: number | string,
     options?: IRequestOptions,
   ): Promise<Response<T>>;
 
-  fetchAll<T extends IJsonapiModel = IJsonapiModel>(
+  fetchAll(
     options?: IRequestOptions,
   ): Promise<Response<T>>;
 }
