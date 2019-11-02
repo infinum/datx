59623ba80afeeaacaa3a8dd9199961b6550e59a0
Darko Kukovec <darko@infinum.hr>
2019-11-02 12:19:30 +0100

    Optimize the builds with terser

--- packages/datx-utils/rollup.config.js
+++ packages/datx-utils/rollup.config.js
@@ -1,31 +1,30 @@
 import typescript from 'rollup-plugin-typescript2';
-import {uglify} from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
-export default [{
-  input: './src/index.ts',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-  ],
-  plugins: [
-    typescript({
-      check: true,
-      typescript: require('typescript'),
-      tsconfig: './tsconfig.build.json',
-    }),
-    uglify(),
-  ]
-}, {
-  input: './src/index.ts',
-  output: [
-    { file: pkg.module, format: 'es' },
-  ],
-  plugins: [
-    typescript({
-      check: true,
-      typescript: require('typescript'),
-      tsconfig: './tsconfig.build.json',
-    }),
-  ]
-}]
+export default [
+  {
+    input: './src/index.ts',
+    output: [{ file: pkg.main, format: 'cjs' }],
+    plugins: [
+      typescript({
+        check: true,
+        typescript: require('typescript'),
+        tsconfig: './tsconfig.build.json',
+      }),
+      terser({ toplevel: true }),
+    ],
+  },
+  {
+    input: './src/index.ts',
+    output: [{ file: pkg.module, format: 'es' }],
+    plugins: [
+      typescript({
+        check: true,
+        typescript: require('typescript'),
+        tsconfig: './tsconfig.build.json',
+      }),
+    ],
+  },
+];
