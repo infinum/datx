9b5efab8efbff1a090a7661fd7d8c00609dd991d
Danijel Buhin <danijel@danijelbuhin.com>
2020-01-28 09:35:15 +0100

    Add more API references, unlock more docusaurus features in siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,18 +8,6 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: "User1",
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: "/img/undraw_open_source.svg",
-    infoLink: "https://www.facebook.com",
-    pinned: true
-  }
-];
-
 const siteConfig = {
   title: "datx", // Title for your website.
   tagline: "A mobx data store",
@@ -37,18 +25,14 @@ const siteConfig = {
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    { doc: "installation", label: "Docs" },
-    { page: "help", label: "Help" },
-    { search: true }
-  ],
+  headerLinks: [{ doc: "installation", label: "Docs" }, { search: true }],
 
   // If you have users set above, you add it here:
-  users,
+  users: [],
 
   /* path to images for header/footer */
-  headerIcon: "img/favicon.ico",
-  footerIcon: "img/favicon.ico",
+  headerIcon: "",
+  footerIcon: "",
   favicon: "img/favicon.ico",
 
   /* Colors for website */
@@ -76,7 +60,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "atom-one-light"
+    theme: "atom-one-dark"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
@@ -89,17 +73,17 @@ const siteConfig = {
 
   // Open Graph and Twitter card images.
   ogImage: "img/undraw_online.svg",
-  twitterImage: "img/undraw_tweetstorm.svg"
+  twitterImage: "img/undraw_tweetstorm.svg",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+  docsSideNavCollapsible: true,
 
   // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+  enableUpdateBy: true,
 
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
