a2772293ba3f82c8202b1814ea4a4fec86915b50
Danijel Buhin <danijel@danijelbuhin.com>
2020-05-28 16:50:16 +0200

    Show version number by the logo

--- website/pages/en/versions.js
+++ website/pages/en/versions.js
@@ -38,7 +38,7 @@ function Versions(props) {
                   <a
                     href={`${siteConfig.baseUrl}${siteConfig.docsUrl}/${
                       props.language ? props.language + '/' : ''
-                    }doc1`}>
+                    }getting-started/installation`}>
                     Documentation
                   </a>
                 </td>
