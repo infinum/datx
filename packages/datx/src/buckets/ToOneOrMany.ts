9b9498999eeb58dee51ac837234102cc364b62ef
Darko Kukovec <darko@infinum.hr>
2020-10-03 17:33:32 +0200

    Add mobx@6 support to network and jsonapi

--- packages/datx/src/buckets/ToOneOrMany.ts
+++ packages/datx/src/buckets/ToOneOrMany.ts
@@ -1,5 +1,5 @@
 import { isArrayLike, makeObservable } from 'datx-utils';
-import { computed, observable } from 'mobx';
+import { computed, observable, runInAction } from 'mobx';
 
 import { IModelRef } from '../interfaces/IModelRef';
 import { PureCollection } from '../PureCollection';
@@ -24,26 +24,28 @@ export class ToOneOrMany<T extends PureModel> {
     protected __skipMissing = true,
   ) {
     makeObservable(this);
-    this.__isList = isArrayLike(data);
-    if (this.__isList) {
-      this.__toManyBucket = new ToMany(
-        data as Array<T | IModelRef>,
-        __collection,
-        __readonly,
-        __model,
-        __key,
-        __skipMissing,
-      );
-    } else {
-      this.__toOneBucket = new ToOne<T>(
-        data as T,
-        __collection,
-        __readonly,
-        __model,
-        __key,
-        __skipMissing,
-      );
-    }
+    runInAction(() => {
+      this.__isList = isArrayLike(data);
+      if (this.__isList) {
+        this.__toManyBucket = new ToMany(
+          data as Array<T | IModelRef>,
+          __collection,
+          __readonly,
+          __model,
+          __key,
+          __skipMissing,
+        );
+      } else {
+        this.__toOneBucket = new ToOne<T>(
+          data as T,
+          __collection,
+          __readonly,
+          __model,
+          __key,
+          __skipMissing,
+        );
+      }
+    });
   }
 
   public setCollection(value: PureCollection | undefined): void {
@@ -62,49 +64,53 @@ export class ToOneOrMany<T extends PureModel> {
   }
 
   public set value(data: T | Array<T> | null) {
-    this.__isList = isArrayLike(data);
-    if (this.__isList) {
-      if (this.__toManyBucket) {
-        this.__toManyBucket.value = data as Array<T>;
+    runInAction(() => {
+      this.__isList = isArrayLike(data);
+      if (this.__isList) {
+        if (this.__toManyBucket) {
+          this.__toManyBucket.value = data as Array<T>;
+        } else {
+          this.__toManyBucket = new ToMany(data as Array<T | IModelRef>, this.__collection);
+        }
+      } else if (this.__toOneBucket) {
+        this.__toOneBucket.value = data as T;
       } else {
-        this.__toManyBucket = new ToMany(data as Array<T | IModelRef>, this.__collection);
+        this.__toOneBucket = new ToOne<T>(data as T, this.__collection);
       }
-    } else if (this.__toOneBucket) {
-      this.__toOneBucket.value = data as T;
-    } else {
-      this.__toOneBucket = new ToOne<T>(data as T, this.__collection);
-    }
+    });
   }
 
   // An ugly workaround to still be able to update the response buckets
   // @ts-ignore
   private set __readonlyValue(data: T | Array<T> | null) {
-    this.__isList = isArrayLike(data);
-    if (this.__isList) {
-      if (this.__toManyBucket) {
+    runInAction(() => {
+      this.__isList = isArrayLike(data);
+      if (this.__isList) {
+        if (this.__toManyBucket) {
+          // @ts-ignore
+          const readonlyStatus = this.__toManyBucket.__readonly;
+
+          // @ts-ignore
+          this.__toManyBucket.__readonly = false;
+          this.__toManyBucket.value = data as Array<T>;
+          // @ts-ignore
+          this.__toManyBucket.__readonly = readonlyStatus;
+        } else {
+          this.__toManyBucket = new ToMany(data as Array<T | IModelRef>, this.__collection);
+        }
+      } else if (this.__toOneBucket) {
         // @ts-ignore
-        const readonlyStatus = this.__toManyBucket.__readonly;
+        const readonlyStatus = this.__toOneBucket.__readonly;
 
         // @ts-ignore
-        this.__toManyBucket.__readonly = false;
-        this.__toManyBucket.value = data as Array<T>;
+        this.__toOneBucket.__readonly = false;
+        this.__toOneBucket.value = data as T;
         // @ts-ignore
-        this.__toManyBucket.__readonly = readonlyStatus;
+        this.__toOneBucket.__readonly = readonlyStatus;
       } else {
-        this.__toManyBucket = new ToMany(data as Array<T | IModelRef>, this.__collection);
+        this.__toOneBucket = new ToOne<T>(data as T, this.__collection);
       }
-    } else if (this.__toOneBucket) {
-      // @ts-ignore
-      const readonlyStatus = this.__toOneBucket.__readonly;
-
-      // @ts-ignore
-      this.__toOneBucket.__readonly = false;
-      this.__toOneBucket.value = data as T;
-      // @ts-ignore
-      this.__toOneBucket.__readonly = readonlyStatus;
-    } else {
-      this.__toOneBucket = new ToOne<T>(data as T, this.__collection);
-    }
+    });
   }
 
   @computed
