b552e6b9e98449753b8f990f077cb485727afdb8
Ivica Batinic <ivica.batinic@infinum.hr>
2021-03-10 00:14:36 +0100

    Fix isomorphic-fetch import in old examples

--- examples/datx-jsonapi-typescript/network.ts
+++ examples/datx-jsonapi-typescript/network.ts
@@ -1,5 +1,5 @@
 import { config } from '@datx/jsonapi';
-import * as fetch from './isomorphic-fetch'; // Or any other fetch lib
+import * as fetch from 'isomorphic-fetch'; // Or any other fetch lib
 
 // Don't need if fetch is polyfilled before the lib is loaded
 config.fetchReference = fetch;
