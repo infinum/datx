239d3f6967fef4df5f5c1658c7522de165fd93c0
Darko Kukovec <darko@infinum.com>
2021-08-19 14:43:12 +0200

    Remove obsolete packages

--- packages/datx-network-promise/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { BaseRequest } from './BaseRequest';
-export { fetchInterceptor } from './interceptors/fetch';
-export { promiseNetwork } from './mixin';
--- packages/datx-network-promise/src/interfaces/IResponseType.ts
+++
@@ -1,4 +0,0 @@
-import { PureModel } from '@datx/core';
-import { Response } from '../Response';
-
-export type IResponseType<TResponseValue extends TModel | Array<TModel>, TModel = PureModel> = Promise<Response<TResponseValue, TModel>>;
