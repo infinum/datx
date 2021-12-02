651580381d762d406dcdfab517c7aba66b1a826a
Filip Voska <filip.voska@infinum.com>
2021-12-02 11:13:40 +0100

    Update tests setup

--- packages/datx-jsonapi-angular/setup-jest.ts
+++ packages/datx-jsonapi-angular/setup-jest.ts
@@ -1,1 +1,17 @@
 import 'jest-preset-angular/setup-jest';
+
+import { mobx } from '@datx/utils';
+
+import mobxInstance from './projects/datx-jsonapi-angular/test/mobx';
+
+if (parseInt(process.env['MOBX_VERSION'] || '0', 10) < 0) {
+  mobx.useMobx(false);
+}
+
+if ('configure' in mobxInstance) {
+  // @ts-ignores
+  mobxInstance.configure({
+    enforceActions: 'observed',
+    // computedRequiresReaction: true,
+  });
+}
