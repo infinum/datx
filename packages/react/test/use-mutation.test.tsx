d300edc9b8006a61e953c9314c2ec5dfa149f28d
Ivica Batinic <ivica.batinic@infinum.hr>
2021-12-10 12:15:45 +0100

    Add use mutation tests

---
+++ packages/react/test/use-mutation.test.tsx
@@ -0,0 +1,105 @@
+import React, { FC } from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { useMutation } from '../src';
+import { renderWithConfig } from './utils';
+import { createClient } from './datx';
+
+interface ITesterProps {
+  mutationFn: () => Promise<any>;
+  onMutate: () => any;
+  onSuccess: () => any;
+  onFailure: () => any;
+  onSettled: () => any;
+}
+
+const Tester: FC<ITesterProps> = ({ mutationFn, onMutate, onSuccess, onFailure, onSettled }) => {
+  const [mutate, { status }] = useMutation(mutationFn, {
+    onMutate,
+    onSuccess,
+    onFailure,
+    onSettled,
+  });
+
+  return <button onClick={() => mutate('test-1')}>{status}</button>;
+};
+
+describe('useMutation', () => {
+  test('should call all the correct function for a successful mutation', async () => {
+    const mutationFn = jest.fn(() => Promise.resolve('result-1'));
+    const onMutate = jest.fn();
+    const onSuccess = jest.fn();
+    const onFailure = jest.fn();
+    const onSettled = jest.fn();
+
+    const client = createClient();
+
+    renderWithConfig(
+      <Tester
+        mutationFn={mutationFn}
+        onMutate={onMutate}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        onSettled={onSettled}
+      />,
+    );
+
+    userEvent.click(screen.getByRole('button'));
+
+    await screen.findByText('running');
+
+    expect(mutationFn).toHaveBeenCalledWith(client, 'test-1');
+    expect(onMutate).toHaveBeenCalledWith({ input: 'test-1' });
+    expect(onSuccess).toHaveBeenCalledWith({
+      data: 'result-1',
+      input: 'test-1',
+    });
+    expect(onFailure).not.toHaveBeenCalled();
+    expect(onSettled).toHaveBeenCalledWith({
+      status: 'success',
+      data: 'result-1',
+      input: 'test-1',
+    });
+  });
+
+  test('should call all the correct function for a failure mutation', async () => {
+    const noop = jest.fn();
+    const mutationFn = jest.fn(() => Promise.reject('reason-1'));
+    const onMutate = jest.fn(() => noop);
+    const onSuccess = jest.fn();
+    const onFailure = jest.fn();
+    const onSettled = jest.fn();
+
+    const client = createClient();
+
+    renderWithConfig(
+      <Tester
+        mutationFn={mutationFn}
+        onMutate={onMutate}
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        onSettled={onSettled}
+      />
+    );
+
+    userEvent.click(screen.getByRole('button'));
+
+    await screen.findByText('running');
+
+    expect(mutationFn).toHaveBeenCalledWith(client, 'test-1');
+    expect(onMutate).toHaveBeenCalledWith({ input: 'test-1' });
+    expect(onSuccess).not.toHaveBeenCalled();
+    expect(onFailure).toHaveBeenCalledWith({
+      error: 'reason-1',
+      rollback: noop,
+      input: 'test-1',
+    });
+    expect(onSettled).toHaveBeenCalledWith({
+      status: 'failure',
+      error: 'reason-1',
+      input: 'test-1',
+      rollback: noop,
+    });
+  });
+});
--- packages/react/test/use-query.test.tsx
+++ packages/react/test/use-query.test.tsx
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { screen } from '@testing-library/react';
 import { server } from './mocks/server';
 
-import { renderWithConfig } from './utils';
+import { getErrorMessage, renderWithConfig } from './utils';
 import { todosError, todosErrorDetails } from './mocks/todos';
 import { message } from './mocks/handlers';
-import { loadingMessage, Todos } from './components/Todos';
+
+import { getModelEndpointUrl } from "@datx/jsonapi";
+import { createQuery } from "@datx/react";
+import { Todo } from './models/Todo';
+import { useQuery } from '../src';
+
+
+interface IQueryTodoVariables { shouldFetch?: boolean }
+
+export const queryTodos = createQuery((client, variables: IQueryTodoVariables) => {
+  const model = new Todo();
+  const key = variables.shouldFetch ? getModelEndpointUrl(model) : null;
+
+  return {
+    key,
+    fetcher: (url: string) => client.request<Todo, Array<Todo>>(url, 'GET')
+  };
+});
+
+const loadingMessage = 'Loading...';
+
+interface ITesterProps { shouldFetch?: boolean }
+
+const Tester: FC<ITesterProps> = ({ shouldFetch = true }) => {
+  const { data, error } = useQuery(queryTodos, { variables: { shouldFetch } });
+
+  if (error) {
+    return <div>{getErrorMessage(error)}</div>;
+  }
+
+  if (!data) {
+    return <div>{loadingMessage}</div>;
+  }
+
+  return <div>{data.data[0].message}</div>;
+};
 
 
 describe('useQuery', () => {
   it('should render data', async () => {
-    renderWithConfig(<Todos />);
+    renderWithConfig(<Tester />);
     screen.getByText(loadingMessage);
 
     await screen.findByText(message);
   });
 
   it('should conditionally fetch data', async () => {
-    renderWithConfig(<Todos shouldFetch={false} />);
+    renderWithConfig(<Tester shouldFetch={false} />);
 
     await screen.getByText(loadingMessage);
   });
@@ -25,7 +60,7 @@ describe('useQuery', () => {
   it('should handle errors', async () => {
     server.use(todosError);
 
-    renderWithConfig(<Todos />);
+    renderWithConfig(<Tester />);
     screen.getByText(loadingMessage);
 
     await screen.findByText(todosErrorDetails);
