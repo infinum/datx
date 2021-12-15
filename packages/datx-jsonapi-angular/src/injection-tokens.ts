958d58c120344c002aa9a3b02c376cb05beae40d
Filip Voska <filip.voska@infinum.com>
2021-12-15 13:33:02 +0100

    Update packages/datx-jsonapi-angular/src/injection-tokens.ts
    Co-authored-by: Kristijan Bambir <kristijan.bambir@infinum.com>

--- packages/datx-jsonapi-angular/src/injection-tokens.ts
+++ packages/datx-jsonapi-angular/src/injection-tokens.ts
@@ -3,6 +3,6 @@ import { IConfigType } from '@datx/jsonapi/dist/NetworkUtils';
 
 export const DATX_CONFIG = new InjectionToken<Partial<IConfigType>>('DATX_CONFIG');
 
-// Use any because there is currently no good way to type the decorated application-specifig Collection
+// Use any because there is currently no good way to type the decorated application-specific Collection
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const APP_COLLECTION = new InjectionToken<any>('APP_COLLECTION');
