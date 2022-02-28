de52602f0710526cba530dc9c2f8f01def74cbdf
Danijel Buhin <danijel@danijelbuhin.com>
2022-02-28 10:29:24 +0100

    fetchQuery tests

--- packages/swr/test/datx.ts
+++ packages/swr/test/datx.ts
@@ -1,11 +1,36 @@
-import { Collection } from "@datx/core";
-import { jsonapiCollection, config } from "@datx/jsonapi";
+import { Collection } from '@datx/core';
+import { jsonapiCollection, config, IJsonapiModel, Response } from '@datx/jsonapi';
 
-import { BASE_URL } from "./constants";
-import { Todo } from "./models/Todo";
+import { Expression, createFetcher } from '../src';
+import { BASE_URL } from './constants';
+import { Todo } from './models/Todo';
 
-class Client extends jsonapiCollection(Collection) {
+export class Client extends jsonapiCollection(Collection) {
   public static types = [Todo];
+
+  private _fallback = new Map();
+
+  public async fetchQuery<TModel extends IJsonapiModel>(expression: Expression<TModel>) {
+    try {
+      const response = await createFetcher(this)(expression);
+      this._fallback.set(expression?.type.type, response.data);
+
+      return {
+        // Fix this after #312 productive
+        [expression?.type.type as string]: response,
+      };
+    } catch (error) {
+      if (error instanceof Response) {
+        throw error.error;
+      }
+
+      throw error;
+    }
+  }
+
+  public get fallback() {
+    return Object.fromEntries(this._fallback);
+  }
 }
 
 export function createClient() {
--- packages/swr/test/fetch-query.test.ts
+++ packages/swr/test/fetch-query.test.ts
@@ -1,7 +1,7 @@
-import { fetchQuery, Client } from '../src';
-import { createClient } from './datx';
+import { createClient, Client } from './datx';
 import { server } from './mocks/server';
 import { todosError } from './mocks/todos';
+import { Todo } from './models/Todo';
 import { queryTodos } from './queries';
 
 describe('fetchQuery', () => {
@@ -12,39 +12,27 @@ describe('fetchQuery', () => {
   });
 
   test('should fetch query', async () => {
-    const todos = await fetchQuery(client, queryTodos, { shouldFetch: true });
+    const response = await client.fetchQuery(queryTodos);
 
-    const key = 'http://localhost:3000/todos';
-    const rawResponse = {
-      response: {
-        data: {
-          data: [
-            {
-              attributes: {
-                message: 'JSON:API paints my bikeshed!',
-              },
-              id: '1',
-              type: 'todos',
-            },
-          ],
-        },
-        headers: [
-          ['content-type', 'application/json'],
-          ['x-powered-by', 'msw'],
-        ],
-        status: 200,
-      },
-    };
+    const key = Todo.type;
 
-    expect(todos).toStrictEqual({
-      [key]: rawResponse,
-    });
+    expect(response[key].data).toBeTruthy();
+    expect((response[key].data as Todo[]).length).toBe(1);
+  });
+
+  test('client stores fallback under the appropriate key', async () => {
+    const key = Todo.type;
+    await client.fetchQuery(queryTodos);
+
+    expect(Object.keys(client.fallback)[0]).toBe(key);
+    expect(client.fallback[key]).toBeTruthy();
+    expect(client.fallback[key].length).toBe(1);
   });
 
   test('should throw on API error', async () => {
     server.use(todosError);
 
-    await expect(fetchQuery(client, queryTodos, { shouldFetch: true })).rejects.toStrictEqual([
+    await expect(client.fetchQuery(queryTodos)).rejects.toStrictEqual([
       {
         detail: 'Not authorized on Sundays.',
         status: '403',
@@ -52,13 +40,13 @@ describe('fetchQuery', () => {
     ]);
   });
 
-  describe('Conditional Data Fetching', () => {
-    test('should throw if variables are used inside query but they are not provided', async () => {
-      await expect(fetchQuery(client, queryTodos)).rejects.toThrow();
-    });
+  // describe('Conditional Data Fetching', () => {
+  //   test('should throw if variables are used inside query but they are not provided', async () => {
+  //     await expect(client.fetchQuery(queryTodos)).rejects.toThrow();
+  //   });
 
-    test('should throw if variables are used inside query but properties are undefined', async () => {
-      await expect(fetchQuery(client, queryTodos, {})).rejects.toThrow();
-    });
-  });
+  //   test('should throw if variables are used inside query but properties are undefined', async () => {
+  //     await expect(client.fetchQuery(queryTodos)).rejects.toThrow();
+  //   });
+  // });
 });
