365f6ccc0d3db7b486bb71537c402183f6305f69
Ivica Batinic <ivica.batinic@infinum.hr>
2022-04-12 11:20:48 +0200

    Fix middleware typing

--- packages/swr/src/middleware.tsx
+++ packages/swr/src/middleware.tsx
@@ -3,5 +3,9 @@ import { Middleware, SWRHook } from 'swr';
 export const middleware: Middleware = (useSWRNext: SWRHook) => (key, fetcher, config) => {
   const { networkConfig, ...swrConfig } = (config as any) || {};
 
-  return useSWRNext(key, (expression) => fetcher?.(expression, { networkConfig }), swrConfig);
+  return useSWRNext(
+    key,
+    fetcher && ((expression) => fetcher(expression, { networkConfig })),
+    swrConfig,
+  );
 };
