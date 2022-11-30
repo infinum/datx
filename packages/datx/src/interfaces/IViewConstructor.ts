ef129b538ef70a55813651e2e4d1c7b216ee54dd
Dani Pavic <dani.pavic@infinum.com>
2022-11-30 15:30:01 +0100

    Fix IViewConstructor type

--- packages/datx/src/interfaces/IViewConstructor.ts
+++ packages/datx/src/interfaces/IViewConstructor.ts
@@ -5,7 +5,7 @@ import { IIdentifier } from './IIdentifier';
 import { IModelConstructor } from './IModelConstructor';
 import { IType } from './IType';
 
-export type IViewConstructor<T, V = View> = new (
+export type IViewConstructor<T extends PureModel, V = View> = new (
   modelType: IModelConstructor<T> | IType,
   collection: PureCollection,
   sortMethod?: string | ((item: T) => any),
