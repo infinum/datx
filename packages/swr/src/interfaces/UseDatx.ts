3a741192d5f58d9d026fbb9c8308c06a561190e4
Kristian Djaković <kristian.djakovic@infinum.hr>
2022-12-14 00:12:14 +0100

    Remove leftover comments

--- packages/swr/src/interfaces/UseDatx.ts
+++ packages/swr/src/interfaces/UseDatx.ts
@@ -5,26 +5,7 @@ export type Model<TExpression extends Expression> = FindModel<
   ExactExpressionArgument<TExpression>['type']
 >;
 
-// export type Relation<TExpression extends Expression> =
-//   ExactExpressionArgument<TExpression> extends { readonly op: 'getRelatedResource' }
-//     ? FindModel<ExactExpressionArgument<TExpression>['relation']>
-//     : ExactExpressionArgument<TExpression> extends { readonly op: 'getRelatedResources' }
-//     ? FindModel<ExactExpressionArgument<TExpression>['relation']>
-//     : never;
-
 export type Data<
   TExpression extends Expression,
   TModel extends IJsonapiModel = Model<TExpression>,
 > = ExactExpressionArgument<TExpression> extends { readonly op: 'getOne' } ? TModel : Array<TModel>;
-
-// export type Data<
-//   TExpression extends Expression,
-//   TModel extends IJsonapiModel = Model<TExpression>,
-//   // TRelationModel extends IJsonapiModel = Relation<TExpression>,
-// > = ExactExpressionArgument<TExpression> extends { readonly op: 'getOne' }
-//   ? TModel
-//   : ExactExpressionArgument<TExpression> extends { readonly op: 'getRelatedResource' }
-//   ? TModel[ExactExpressionArgument<TExpression>['relation']]
-//   : ExactExpressionArgument<TExpression> extends { readonly op: 'getRelatedResources' }
-//   ? Array<TModel[ExactExpressionArgument<TExpression>['relation']]>
-//   : Array<TModel>;
