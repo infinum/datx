ed9d5fc138bad976f26e11b4af653d2ee704a6e9
Ivica Batinic <ivica.batinic@infinum.hr>
2023-03-23 11:32:19 +0100

    Add modelToJsonApi relationship test

--- packages/datx-test-data-factory/test/jsonapi/issues.test.ts
+++ packages/datx-test-data-factory/test/jsonapi/issues.test.ts
@@ -2,6 +2,7 @@ import { createJsonapiClient } from './create-client';
 import { createFactory } from '../../src';
 import { User } from './models/User';
 import { modelToJsonApi } from '@datx/jsonapi';
+import { Comment } from './models/Comment';
 
 const client = createJsonapiClient();
 const factory = createFactory(client);
@@ -21,6 +22,27 @@ describe('jsonapi issues', () => {
     const user = userFactory();
 
     const jsonapiUser = modelToJsonApi(user);
+
+    expect(jsonapiUser.id).toBe('1');
+  });
+
+  it('should convert relationship model to json:api payload with correct id', () => {
+    const userFactory = factory(User, {
+      fields: {
+        id: 1,
+      },
+    });
+
+    const commentFactory = factory(Comment, {
+      fields: {
+        id: 1,
+        author: userFactory(),
+      },
+    });
+
+    const comment = commentFactory();
+
+    const jsonapiUser = modelToJsonApi(comment.author);
 
     expect(jsonapiUser.id).toBe('1');
   });
