c8395d82646a66faa696525ca01654550c0e36ee
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-05-02 14:57:43 +0200

    Fix duplicated queries issue

--- packages/datx-swr/test/issues.test.tsx
+++ packages/datx-swr/test/issues.test.tsx
@@ -1,3 +1,4 @@
+import { config } from '@datx/jsonapi';
 import { createFetcher } from '@datx/swr';
 import { createClient } from './datx';
 
@@ -32,5 +33,39 @@ describe('issues', () => {
     );
 
     expect(requestSpy).toBeCalledTimes(2);
+  });
+
+  it('should not duplicate query params when relation query is used', async () => {
+    const client = createClient();
+    const fetcher = createFetcher(client);
+
+    const fetchSpy = jest.spyOn(config, 'fetchReference');
+
+    await fetcher({
+      op: 'getRelatedResources',
+      type: 'withEndpoint',
+      id: '1',
+      relation: 'related',
+      queryParams: {
+        custom: ['custom=1'],
+      },
+    });
+
+    expect(fetchSpy).toBeCalledWith(expect.stringMatching('\\?custom=1$'), expect.anything());
+
+    await fetcher({
+      op: 'getRelatedResource',
+      type: 'withEndpoint',
+      id: '1',
+      relation: 'related',
+      queryParams: {
+        custom: ['custom=1'],
+      },
+    });
+
+    expect(fetchSpy).toHaveBeenLastCalledWith(
+      expect.stringMatching('\\?custom=1$'),
+      expect.anything(),
+    );
   });
 });
