632ca94c9e7361ff84d5d397f616b4358cde7456
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-05-10 21:44:17 +0200

    Extend the fetch query configuration interface

--- packages/datx-swr/src/interfaces/IFetchQueryConfiguration.ts
+++ packages/datx-swr/src/interfaces/IFetchQueryConfiguration.ts
@@ -2,5 +2,12 @@ export interface IFetchQueryConfiguration {
   /**
    * fetchQuery will not throw on error
    */
-  prefetch: boolean | ((res: unknown) => boolean);
+  prefetch?: boolean | ((res: unknown) => boolean);
+
+  /**
+   * It should save the response to the cache so it can be hydrated on the client
+   *
+   * @default true
+   */
+  hydrate?: boolean;
 }
