2eef097a2fb32f6cda31dec50d27220266a99d76
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-08-03 14:21:05 +0200

    Refactor hydrate

--- packages/datx-swr/src/hydrate.tsx
+++ packages/datx-swr/src/hydrate.tsx
@@ -1,5 +1,5 @@
+import { IGetAllResponse, IJsonapiModel, Response } from '@datx/jsonapi';
 import { PropsWithChildren } from 'react';
-import { IJsonapiModel, Response, IGetAllResponse } from '@datx/jsonapi';
 import { SWRConfig } from 'swr';
 import { useClient } from './hooks/useClient';
 import { IClientInstance } from './interfaces/Client';
@@ -18,19 +18,23 @@ const hydrate = (client: IClientInstance, fallback: Fallback | undefined) => {
     const response = fallback[currentValue];
 
     if (Array.isArray(response)) {
-      previousValue[currentValue] = response.reduce(
-        (fallbackValue, rawResponse) => {
-          const res = new Response(rawResponse, client);
+      previousValue[currentValue] = response.map((res) => new Response(res, client));
+    } else if ('op' in response) {
+      if (response.op === 'getAll') {
+        previousValue[currentValue] = response.data.reduce(
+          (fallbackValue, rawResponse) => {
+            const res = new Response(rawResponse, client);
 
-          fallbackValue.responses.push(res);
-          fallbackValue.data.push(...(res.data as Array<IJsonapiModel>));
+            fallbackValue.responses.push(res);
+            fallbackValue.data.push(...(res.data as Array<IJsonapiModel>));
 
-          return fallbackValue;
-        },
-        { data: [], responses: [] } as Omit<IGetAllResponse<IJsonapiModel>, 'lastResponse'>,
-      );
-      previousValue[currentValue].lastResponse =
-        previousValue[currentValue].responses[response.length - 1];
+            return fallbackValue;
+          },
+          { data: [], responses: [] } as Omit<IGetAllResponse<IJsonapiModel>, 'lastResponse'>,
+        );
+        previousValue[currentValue].lastResponse =
+          previousValue[currentValue].responses[response.data.length - 1];
+      }
     } else {
       previousValue[currentValue] = new Response(response, client);
     }
--- packages/datx-swr/src/interfaces/Fallback.ts
+++ packages/datx-swr/src/interfaces/Fallback.ts
@@ -2,4 +2,7 @@ import { IRawResponse } from '@datx/jsonapi';
 
 export type FallbackResponse = Omit<IRawResponse, 'collection'>;
 
-export type Fallback = Record<string, FallbackResponse | Array<FallbackResponse>>;
+export type Fallback = Record<
+  string,
+  FallbackResponse | Array<FallbackResponse> | { op: 'getAll'; data: Array<FallbackResponse> }
+>;
